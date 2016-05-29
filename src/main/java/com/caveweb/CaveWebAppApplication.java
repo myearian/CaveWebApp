957954b5c75c47d2006cee306ea00c1bdf1f1a47
Matt Yearian <ramshambo2001@gmail.com>
2016-05-28 21:54:41 -0500

    Initial Commit, project creation.

---
+++ src/main/java/com/caveweb/CaveWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.caveweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaveWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaveWebAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/caveweb/CaveWebAppApplicationTests.java
@@ -0,0 +1,18 @@
+package com.caveweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CaveWebAppApplication.class)
+@WebAppConfiguration
+public class CaveWebAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
